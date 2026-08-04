1f498a4720eaa45a02e95a6c1ebb83a323c69855
Clovis Descombes <descombsclovis@gmail.com>
2026-08-04 14:06:46 +0200

    Test X OAuth

--- src/pages/CustomerLogin.jsx
+++ src/pages/CustomerLogin.jsx
@@ -244,41 +244,9 @@ export default function CustomerLogin() {
               Continuer avec Google
 
             </button>
-<button
-  type="button"
-  onClick={() => signInWithApple()}
-  className="w-full h-14 rounded-2xl bg-black hover:bg-neutral-800 text-white transition-all flex items-center justify-center gap-3 font-semibold"
->
 
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 384 512"
-    className="w-5 h-5 fill-current"
-  >
-    <path d="M318.7 268.7c-.2-36.7 16.4-64.4 49.8-84.8-18.6-26.6-46.8-41.2-84.2-44.2-35.3-2.8-73.8 20.8-87.9 20.8-14.9 0-48.8-19.8-76.6-19.8C61.6 141 0 188.3 0 286.2c0 28.9 5.3 58.8 15.9 89.6 14.1 40.6 65.1 140 118.3 138.4 27.8-.7 47.4-19.8 83.6-19.8 35.1 0 53.3 19.8 84.3 19.8 53.7-.8 99.8-91.1 113.2-131.8-77.6-36.5-96.6-106.5-96.6-113.7zM261.7 96.3c27.1-32.1 24.6-61.3 23.8-71.8-23.9 1.4-51.5 16.3-67.3 34.7-17.4 20-27.7 44.7-25.5 72.1 26 .2 51.7-13.2 69-35z"/>
-  </svg>
 
-  Continuer avec Apple
-
-</button>
-
-           <button
-  type="button"
-  onClick={() => signInWithX()}
-  className="w-full h-14 rounded-2xl bg-neutral-900 hover:bg-black text-white transition-all flex items-center justify-center gap-3 font-semibold"
->
-
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 1200 1227"
-    className="w-5 h-5 fill-current"
-  >
-    <path d="M714 519L1160 0H1054L667 450L358 0H0L468 681L0 1227H106L515 752L842 1227H1200L714 519Z"/>
-  </svg>
-
-  Continuer avec X
-
-</button>
+           
                         <div className="border-t border-gray-200 pt-7 mt-7 text-center">
               <p className="text-sm text-gray-500">
                 Vous n’avez pas encore de compte ?
